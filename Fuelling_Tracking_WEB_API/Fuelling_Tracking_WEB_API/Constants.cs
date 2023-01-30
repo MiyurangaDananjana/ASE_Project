e7b7e03873fcfc8c53ac731cbdc5fa063775c723
MiyurangaDananjana <83136587+MiyurangaDananjana@users.noreply.github.com>
2023-01-30 13:57:35 +0530

    Back End
    The back end of the system can be implemented using ASP.NET Web API Core 7.0, which is a framework for building HTTP services that can be consumed by a broad range of clients, including browsers and mobile devices. The back end will handle all data operations, such as user registration, fuel requests, payment processing, and reporting. The API can use Entity Framework Core 7.0 for data access and management, allowing for easy integration with a database. The API can also implement security features, such as authentication and authorization, to ensure data privacy and secure access to sensitive information. Additionally, the API can implement real-time notifications using SignalR Core 7.0 for efficient and effective communication between the back end and clients.

---
+++ Fuelling_Tracking_WEB_API/Fuelling_Tracking_WEB_API/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Fuelling_Tracking_WEB_API
+{
+    public static class Constants
+    {
+        public const string Audience = "https://localhost:44322";
+        public const string Issuer = "https://localhost:44322";
+        public const string Secret = "vgcsdcsdi7c6vwicvui7wciwevcITCVIyctcvcghgcvhasvdcvydasuc76efwuiCVI7F3WVDHVSCHXVGSCAYDFIWQ73FDI76FSAUCVahscvkcgsvcjaskdcjdsakcdsjcvewfyiwfeew";
+    }
+}
---
+++ Fuelling_Tracking_WEB_API/Fuelling_Tracking_WEB_API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuelling_Tracking_WEB_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet("authenticate")]
+        public IActionResult Authentication()
+        {
+            var claims = new[]
+            {
+                new Claim("FullName", "Last Name"),
+                new Claim(JwtRegisteredClaimNames.Sub, "user_id")
+            };
+
+            var keyBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+
+            var key = new SymmetricSecurityKey(keyBytes);
+
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                Constants.Audience,
+                Constants.Issuer,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddHours(1), signingCredentials);
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new { accessToken = tokenString });
+        }
+        
+
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        [Authorize]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
